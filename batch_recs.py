ef4656aac749b0093d6317e1d623703cfa47c737
Howon Lee <howon@howonlee.com>
2015-10-20 12:33:39 -0700

    thinking about it later

--- batch_recs.py
+++ batch_recs.py
@@ -4,5 +4,20 @@ Prototype for batch recommendations using PGM
 
 import numpy as np
 
+def create_seeds():
+    pass
+
+def pgm():
+    pass
+
+def read_data():
+    pass
+
+def create_skg():
+    """
+    Create a stochastic kronecker graph to be the model to percolate
+    """
+    pass
+
 if __name__ == "__main__":
     pass
