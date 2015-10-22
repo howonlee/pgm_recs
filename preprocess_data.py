a2617c60db181e381e98e2ba8577a8c252ed763d
Howon Lee <howon@howonlee.com>
2015-10-22 12:12:23 -0700

    good results on prelims

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     rating_cards = collections.Counter()
-    with open("data/u.data") as data_file:
+    with open("data/ratings.dat") as data_file:
         for line in data_file:
-            user_id, item_id, rating, timestamp = map(int, line.split())
+            user_id, item_id, rating, timestamp = map(float, line.split("::"))
             rating_cards[user_id] += 1
     ratings = list(reversed(sorted(rating_cards.values())))
     plt.loglog(ratings)
