e0d43e4873fe825784f686ac47c8b2f2ee921dba
Howon Lee <howon@howonlee.com>
2015-11-12 17:50:46 -0800

    thinking about cosines

--- cosine_tests.py
+++ cosine_tests.py
@@ -1,9 +1,20 @@
 import numpy as np
 import networkx as nx
+import matplotlib.pyplot as plt
+
+def wash_words(words):
+    net = nx.Graph()
+    for fst, snd in zip(words, words[1:]):
+        net.add_edge(fst, snd)
+    return net
+
+def cos_hist(net):
+    pass
 
 if __name__ == "__main__":
-    wash the words
-    create cosine matrices
-    look at distributions of cosine values on a histogram
-    try to see if there are familiar patterns
-    pass
+    with open("corpus.txt") as corpus_file:
+        words = corpus_file.read().split()[:5000]
+    word_net = wash_words(words)
+    cos_vals = create_cos_mat(word_net)
+    plt.plot(cos_vals)
+    plt.show()
