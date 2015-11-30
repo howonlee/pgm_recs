5edf1b6e526173b405cdd22ac48cd24b738c8ef0
Howon Lee <howon@howonlee.com>
2015-11-29 20:45:17 -0800

    that seems strangely coherent

--- link_prediction.py
+++ link_prediction.py
@@ -3,15 +3,14 @@ import networkx as nx
 import pgm
 
 def make_prediction(matching, sampled_net, ansatz_net):
-    print matching
-    return []
-    #predictions = []
-    #neighbors is something
-    #matching_set = set([x for _, x in matching])
-    #for sample_elem, ansatz_elem in matching:
-    #    if ansatz elems is in matching_set:
-    #        add the corresponding elements to predictions
-    #return predictions
+    predictions = {}
+    matching_dict = {ansatz_elem:sample_elem for sample_elem, ansatz_elem in matching}
+    for sample_elem, ansatz_elem in matching:
+        ansatz_neighbors = ansatz_net.neighbors(ansatz_elem)
+        sample_neighbors = [matching_dict.get(ansatz_neighbor, None) for ansatz_neighbor in ansatz_neighbors]
+        sample_neighbors = filter(lambda x: x not in sampled_net.neighbors(sample_elem), sample_neighbors)
+        predictions[sample_elem] = sample_neighbors
+    return predictions
 
 def predict_with_ansatz(sampled_net):
     ansatz_net = pgm.skg_like(sampled_net)
@@ -26,7 +25,7 @@ def print_acc(predictions, sampled_net, orig_net):
     raise NotImplementedError()
 
 if __name__ == "__main__":
-    orig_net = nx.read_edge_list("fb_net.txt")
+    orig_net = nx.read_edge_list("net.txt")
     sampled_net = pgm.sample_net(orig_net)
     net_predictions = predict_with_ansatz(sampled_net)
     print_acc(net_predictions, sampled_net, orig_net)
