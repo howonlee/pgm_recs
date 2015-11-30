f750435084b566a9b91baf20a870c8e6f88ea5e4
Howon Lee <howon@howonlee.com>
2015-11-29 21:03:13 -0800

    that did not work so well

--- link_prediction.py
+++ link_prediction.py
@@ -3,6 +3,7 @@ import networkx as nx
 import pgm
 
 def make_prediction(matching, sampled_net, ansatz_net):
+    print "begin predictions..."
     predictions = {}
     matching_dict = {ansatz_elem:sample_elem for sample_elem, ansatz_elem in matching}
     for sample_elem, ansatz_elem in matching:
@@ -13,20 +14,24 @@ def make_prediction(matching, sampled_net, ansatz_net):
     return predictions
 
 def predict_with_ansatz(sampled_net):
+    print "making skg..."
     ansatz_net = pgm.skg_like(sampled_net)
-    seed = pgm.get_biggest_matchings(sampled_net, ansatz_net)
-    matching = pgm.expand_with_something(sampled_net, ansatz_net, seed)
+    print "making seed...."
+    seed = pgm.generate_biggest_matching(sampled_net, ansatz_net, 20)
+    print "making matching...."
+    matching = pgm.expand_when_stuck(sampled_net, ansatz_net, seed)
     return make_predictions(matching, sampled_net, ansatz_net)
 
 def print_acc(predictions, sampled_net, orig_net):
+    print predictions
     """
     This is a bullshit accuracy, oh well
     """
     raise NotImplementedError()
 
 if __name__ == "__main__":
-    orig_net = nx.read_edge_list("net.txt")
-    sampled_net = pgm.sample_net(orig_net)
+    orig_net = nx.read_edgelist("data/net.txt")
+    sampled_net = pgm.select_net(orig_net)
     net_predictions = predict_with_ansatz(sampled_net)
     print_acc(net_predictions, sampled_net, orig_net)
     #if not, then see why, be durable, look real hard at the original 2009 paper
